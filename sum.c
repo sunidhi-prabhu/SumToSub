d0b6ef0af232b7f5d0661b583a97d2f25f72cfdb
Satyam Bharti <bharti.satyamspb@gmail.com>
2023-08-25 22:19:32 +0530

    Added sub and modified user experience

--- sum.c
+++ sum.c
@@ -1,8 +1,20 @@
 #include<stdio.h>
-main()
+#include<stdlib.h>
+void main()
 {
-int a,b,c;
-scanf("%d %d",&a,&b);
-c = a + b;
-printf("the sum is %d",c);
+    int num1,num2,res,choice;
+    printf("Enter two no.s\n");
+    scanf("%d %d",&num1,&num2);
+    printf("\nEnter 1 for sum and 2 for difference: ");
+    scanf("%d",&choice);
+    if(choice==1)
+        res=num1+num2;
+    else if(choice==2)
+        res=num1-num2;
+    else
+    {
+        printf("Invalid choice!!\n");
+        exit(0);
+    }
+    printf("the result is %d\n",res);
 }
